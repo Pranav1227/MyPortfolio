cbb1d7e0072ba196e4b0fe12c48790824177f84e
Pranav Bandagle <147332328+Pranav1227@users.noreply.github.com>
2024-09-22 19:45:26 +0530

    updated the portfolio

--- JS/skill.js
+++ JS/skill.js
@@ -19,3 +19,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 20);
     });
 });
+
+
+// Arrow js 
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        scrollToTopBtn.classList.add('show');
+    } else {
+        scrollToTopBtn.classList.remove('show');
+    }
+};
+
+scrollToTopBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
